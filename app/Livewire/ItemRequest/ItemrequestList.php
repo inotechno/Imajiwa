a3eeb9d64da929e834ef1724c82308692855c330
MuhammadWildanf <wildanf.daftar@gmail.com>
2024-09-10 16:32:42 +0700

    fix: Category Inventory

---
+++ app/Livewire/ItemRequest/ItemrequestList.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\ItemRequest;
+
+use Livewire\Component;
+use Livewire\Attributes\Reactive;
+
+
+class ItemrequestList extends Component
+{
+
+    #[Reactive]
+    public $inventories;
+
+    public function render()
+    {
+        return view('livewire.item-request.itemrequest-list');
+    }
+}
