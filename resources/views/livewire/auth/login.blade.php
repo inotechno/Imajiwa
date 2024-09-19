12bc3ef86600e6837ef81fca20b438aa442d37ff
MuhammadWildanf <wildanf.daftar@gmail.com>
2024-09-20 04:59:10 +0700

    fix: Login Page

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,43 +1,19 @@
-<div>
-    <div class="account-pages my-5 pt-sm-5">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card overflow-hidden">
-                        <div class="bg-primary bg-soft">
-                            <div class="row">
-                                <div class="col-7">
-                                    <div class="text-primary p-4">
-                                        <h5 class="text-primary">Welcome Back !</h5>
-                                        <p>Sign in to continue to {{ config('app.name') }}.</p>
-                                    </div>
-                                </div>
-                                <div class="col-5 align-self-end">
-                                    <img src="{{ asset('images/profile-img.png') }}" alt="" class="img-fluid">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body pt-0">
-                            <div class="auth-logo">
-                                <div class="auth-logo-light">
-                                    <div class="avatar-md profile-user-wid mb-4">
-                                        <span class="avatar-title rounded-circle bg-light">
-                                            <img src="{{ asset('images/logo-light.svg') }}" alt=""
-                                                class="rounded-circle" height="34">
-                                        </span>
-                                    </div>
-                                </div>
+<div class="account-pages d-flex align-items-center" style="min-height: calc(100vh - 40px); background-color: #2e3033;" ">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="card overflow-hidden">
+                    <div class="row g-0">
 
-                                <div class="auth-logo-dark">
-                                    <div class="avatar-md profile-user-wid mb-4">
-                                        <span class="avatar-title rounded-circle bg-light">
-                                            <img src="{{ asset('images/logo.svg') }}" alt=""
-                                                class="rounded-circle" height="34">
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2">
+                        <div class="col-md-5 d-none d-md-flex justify-content-center align-items-center"
+                            style="background-color: #0a0b0b;">
+                            <img src="{{ asset('images/logo-imajiwa.png') }}" alt="Imajiwa Logo"
+                                class="img-fluid p-4" style="max-width: 80%; height: auto;">
+                        </div>
+
+                        <div class="col-12 col-md-7 p-4 d-flex flex-column justify-content-center">
+                            <h4 class="text-white text-center text-md-start mt-3">IMAJIWA Creative Studio</h4>
+                            <div class="p-2 mt-3">
                                 <form class="form-horizontal" wire:submit="login">
 
                                     <div class="mb-3">
@@ -49,8 +25,11 @@
                                     <div class="mb-3">
                                         <label class="form-label">Password</label>
                                         <div class="input-group auth-pass-inputgroup">
-                                            <input type="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon" wire:model="password" autocomplete="current-password">
-                                            <button class="btn btn-light" type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                            <input type="password" class="form-control" placeholder="Enter password"
+                                                aria-label="Password" aria-describedby="password-addon"
+                                                wire:model="password" autocomplete="current-password">
+                                            <button class="btn btn-light" type="button" id="password-addon"><i
+                                                    class="mdi mdi-eye-outline"></i></button>
                                         </div>
                                     </div>
 
@@ -73,20 +52,9 @@
                                     </div>
                                 </form>
                             </div>
-
                         </div>
-                    </div>
-                    <div class="mt-5 text-center">
 
-                        <div>
-                            <p>Don't have an account ? <a href="auth-register.html" class="fw-medium text-primary">
-                                    Signup now </a> </p>
-                            <p>©{{ $year }} EMSv2. Crafted with <i class="mdi mdi-heart text-danger"></i> by
-                                InoTechno
-                            </p>
-                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
--- resources/views/livewire/component/page/footer.blade.php
+++ resources/views/livewire/component/page/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="col-sm-6">
             <script>
                 2024
-            </script> © Skote.
+            </script> © Imajiwa Creative House.
         </div>
-        <div class="col-sm-6">
+        {{-- <div class="col-sm-6">
             <div class="text-sm-end d-none d-sm-block">
                 Design & Develop by Themesbrand
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
