b2ac07a9e8e6ad0a7c539f303e01ceaeb22b90ff
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2026-01-12 22:13:27 +0700

    update

--- resources/views/livewire/project/project-board.blade.php
+++ resources/views/livewire/project/project-board.blade.php
@@ -1,13 +1,15 @@
-<div wire:ignore>
-    <div 
-        id="tldraw-root" 
-        style="height: 80vh; width: 100%; position: relative;"
-        data-props="{{ json_encode([
-            'projectId' => $projectId,
-            'initialSnapshot' => $initialState,
-            'componentId' => $this->getId(),
-            'userId' => auth()->id(),
-            'userName' => auth()->user()?->name ?? 'Guest',
-        ]) }}"
-    ></div>
+<div>
+    <div wire:ignore>
+        <div 
+            id="tldraw-root" 
+            style="height: 80vh; width: 100%; position: relative;"
+            data-props="{{ json_encode([
+                'projectId' => $projectId,
+                'initialSnapshot' => $initialState,
+                'componentId' => $this->getId(),
+                'userId' => auth()->id(),
+                'userName' => auth()->user()?->name ?? 'Guest',
+            ]) }}"
+        ></div>
+    </div>
 </div>
